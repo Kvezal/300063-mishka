f5f56c41e0b120a9cc9a44591c9e201346c93317
Maxim Voropai <muckam49@mail.ru>
2017-06-03 01:33:19 +0800

    The menu has been edited for the mobile version

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var menuList = mainNav.querySelector('.main-nav__list');
 
 
 toggleMenu.classList.remove('main-nav__toggle--nojs');
-menuList.classList.remove('main-nav__list--nojs');
+mainNav.classList.remove('main-nav--nojs');
 
 
 
@@ -14,10 +14,10 @@ toggleMenu.addEventListener('click', function(event) {
 
   if (toggleMenu.classList.contains('main-nav__toggle--open')) {
     toggleMenu.classList.remove('main-nav__toggle--open');
-    menuList.classList.remove('main-nav__list--open');
+    mainNav.classList.remove('main-nav--open');
 
   } else {
     toggleMenu.classList.add('main-nav__toggle--open');
-    menuList.classList.add('main-nav__list--open');
+    mainNav.classList.add('main-nav--open');
   }
 });
